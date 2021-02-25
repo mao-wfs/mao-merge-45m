594e47892437b78b9de2b1851a1846a028749c05
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2021-02-25 20:24:37 +0900

    #1 Add cli module

--- nro45_merge/__init__.py
+++ nro45_merge/__init__.py
@@ -1,3 +1,11 @@
 # flake8: noqa
 __author__ = "Akio Taniguchi"
 __version__ = "0.1.0"
+
+
+# standard library
+from logging import getLogger
+
+
+# package-level logger
+logger = getLogger(__name__)
