28d2fb7f58186f1477ed86430bebd2983be75275
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2021-07-01 23:29:42 +0900

    #7 Add test script for GBD data

--- tests/test_accelerometer.py
+++ tests/test_accelerometer.py
@@ -3,7 +3,8 @@ from pathlib import Path
 
 
 # third-party packages
-from nro45_merge.accelerometer import get_header
+import pandas as pd
+from nro45_merge.accelerometer import get_data, get_header
 
 
 # constants
@@ -17,3 +18,13 @@ def test_get_header():
 
     assert header["Common"]["ID"] == "3502111C"  # first item
     assert header["Waveform"]["XYLine"]["CH4"] == [21, 0, 21]  # last item
+
+
+def test_get_data():
+    """Test whether the data of a GBD file is correctly parsed."""
+    data = get_data(TEST_GBD)
+
+    assert data.columns[0] == "CH1 (mV)"
+    assert data.columns[-1] == "Status (None)"
+    assert data.index[0] == pd.Timestamp("2018-12-02 09:58:42.000000")
+    assert data.index[-1] == pd.Timestamp("2018-12-02 10:28:42.010000")
