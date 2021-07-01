09a4d46c12296ca18c53fbe904d4acef08ccf8ff
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2021-07-01 23:25:05 +0900

    #7 Add GBD data function

--- nro45_merge/accelerometer.py
+++ nro45_merge/accelerometer.py
@@ -1,27 +1,35 @@
 # standard library
 import re
 from pathlib import Path
-from typing import Any, Union, Dict, List
+from struct import Struct
+from typing import Any, BinaryIO, Callable, Dict, List, Optional, Tuple, Union
 
 
 # third-party packages
+import numpy as np
+import pandas as pd
 from tomlkit import parse
+from tqdm import tqdm
 
 
 # constants
+BIG_ENDIAN = ">"
 HEADER_REMOVAL = re.compile(r"\x00| ")
 HEADER_SECTION = re.compile(r"^(\$+)(.+)$")
 HEADER_SIZE = re.compile(r"HeaderSiz\s+=\s+(\d+)")
 HEADER_VALUE = re.compile(r"^\s*(\w+)\s*=\s*(.+)$")
 
 
-# main features
-def get_header(path: Path, encoding: str = "shift-jis") -> Dict[str, Any]:
-    """Return the header of a GBD file as a dictionary."""
-    size = get_header_size(path, encoding)
+# type hints
+Header = Dict[str, Any]
+Data = pd.DataFrame
 
+
+# main features
+def get_header(path: Path, encoding: str = "shift-jis") -> Header:
+    """Return the header of a GBD file as a dictionary."""
     with path.open("rb") as f:
-        header = f.read(size).decode(encoding)
+        header = f.read(get_header_size(path, encoding)).decode(encoding)
         header_rows = HEADER_REMOVAL.sub("", header).split()
 
     toml_rows = []
@@ -33,7 +41,37 @@ def get_header(path: Path, encoding: str = "shift-jis") -> Dict[str, Any]:
     return parse("\n".join(toml_rows))
 
 
-# helper features
+def get_data(
+    path: Path,
+    encoding: str = "shift-jis",
+    progress: bool = False,
+) -> Data:
+    """Return the data of a GBD file as a pandas DataFrame."""
+    header = get_header(path, encoding)
+
+    names = get_data_names(header)
+    units = get_data_units(header)
+    scales = get_data_scales(header)
+    length = get_data_length(header)
+    index = get_data_index(header)
+    read = get_data_reader(header)
+
+    data = np.zeros([length, len(names)])
+
+    with path.open("rb") as f:
+        # skip header
+        f.read(get_header_size(path, encoding))
+
+        for i in tqdm(range(length), disable=not progress):
+            data[i] = read(f)
+
+    data /= scales
+
+    cols = [f"{name} ({unit})" for name, unit in zip(names, units)]
+    return pd.DataFrame(data, index, cols)
+
+
+# helper features (header)
 def get_header_size(path: Path, encoding: str = "shift-jis") -> int:
     """Return the byte size of a GBD file (multiple of 2048)."""
     with path.open("rb") as f:
@@ -82,6 +120,136 @@ def to_parsable(obj: str) -> Union[str, int]:
         return obj.strip('"')
 
 
-if __name__ == "__main__":
-    path = Path("data/2018-12-02_09_58_42.gbd")
-    ret = get_header(path)
+# helper features (data)
+def get_data_names(header: Header) -> List[str]:
+    """Return names (labels) of data."""
+    return header["Common"]["Data"]["Order"]
+
+
+def get_data_length(header: Header) -> int:
+    """Return the number of samples along time axis."""
+    return int(header["Common"]["Data"]["Counts"])
+
+
+def get_data_index(header: Header) -> pd.DatetimeIndex:
+    """Return index of data along time axis."""
+    start = "T".join(header["Common"]["Time"]["Start"])
+    freq = header["Common"]["Data"]["Sample"]
+    periods = get_data_length(header)
+
+    return pd.date_range(start, None, periods, freq)
+
+
+def get_data_units(header: Header) -> List[Optional[str]]:
+    """Return units (V, mV, ...) of data."""
+    names = get_data_names(header)
+    scales = header["Measure"]["Scale"]
+    units = []
+
+    for name in names:
+        if name in scales:
+            units.append(scales[name][6])
+        else:
+            units.append(None)
+
+    return units
+
+
+def get_data_volts(header: Header) -> List[Optional[str]]:
+    """Return voltage range (units of V or mV) of data."""
+    names = get_data_names(header)
+    volts = []
+
+    for name in names:
+        if name in (amp := header["Amp"]):
+            volts.append(amp[name][2])
+        else:
+            volts.append(None)
+
+    return volts
+
+
+def get_data_scales(header: Header) -> List[float]:
+    """Return scale (dimensionless) of data."""
+    names = get_data_names(header)
+    units = get_data_units(header)
+    volts = get_data_volts(header)
+    scales = []
+
+    for name, unit, volt in zip(names, units, volts):
+        if unit is None or volt is None:
+            scales.append(1)
+            continue
+
+        if unit == "ﾟC":
+            scales.append(10)
+            continue
+
+        scale = 1
+
+        if "1" in volt:
+            scale *= 2
+        elif "2" in volt:
+            scale *= 1
+        elif "4" in volt:
+            scale *= 5
+        elif "5" in volt:
+            scale *= 4
+        else:
+            raise ValueError(volt)
+
+        if volt in ("10mV", "20mV"):
+            scale *= 1000
+        elif volt in ("50mV", "100mV", "200mV"):
+            scale *= 100
+        elif volt in ("500mV", "1V", "2V"):
+            scale *= 10
+        elif volt in ("5V", "10V", "20V"):
+            scale *= 1
+        elif volt in ("50V", "100V", "200V"):
+            scale *= 0.1
+        elif volt in ("500V", "1000V"):
+            scale *= 0.01
+        else:
+            raise ValueError(volt)
+
+        if unit == "V":
+            scale *= 1000
+        elif unit == "mV":
+            scale *= 1
+        else:
+            raise ValueError(unit)
+
+        scales.append(scale)
+
+    return scales
+
+
+def get_data_reader(header: Header) -> Callable[[BinaryIO], Tuple]:
+    """Make a binary reader function for data."""
+    names = get_data_names(header)
+
+    format_ = ""
+
+    for name in names:
+        if "CH" in name:
+            format_ += "h"
+        elif "Pulse" in name:
+            format_ += "L"
+        elif "Logic" in name:
+            format_ += "H"
+        elif "Alarm" in name:
+            format_ += "H"
+        elif "AlOut" in name:
+            format_ += "H"
+        elif "Status" in name:
+            format_ += "H"
+        else:
+            raise ValueError(name)
+
+    struct = Struct(BIG_ENDIAN + format_)
+
+    def reader(f: BinaryIO) -> Tuple:
+        return struct.unpack(f.read(struct.size))
+
+    return reader
