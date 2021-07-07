96bffd0ccfb8a105da9e8811fbc066b6367242c9
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2021-07-07 20:02:51 +0900

    #13 Add test script for CSV data

---
+++ tests/test_weather.py
@@ -0,0 +1,33 @@
+# standard library
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
+
+# third-party packages
+import pandas as pd
+import xarray as xr
+from nro45_merge.weather import to_dist_zarr
+
+
+# constants
+CSV_COLS = "time", "wind_speed", "wind_direction"
+TEST_CSV = Path("data") / "speed_dir_20201102.csv"
+
+
+# test functions
+def test_to_dist_zarr():
+    """Test whether a CSV file is correctly parsed."""
+    with TemporaryDirectory() as zarr:
+        path_zarr = Path(zarr)
+        to_dist_zarr(TEST_CSV, path_zarr, overwrite=True)
+
+        ds = xr.open_zarr(path_zarr)
+        df = pd.read_csv(
+            TEST_CSV,
+            names=CSV_COLS,
+            index_col=0,
+            parse_dates=True,
+        )
+
+        assert (ds.wind_speed.to_series() == df[CSV_COLS[1]]).all()
+        assert (ds.wind_direction.to_series() == df[CSV_COLS[2]]).all()
