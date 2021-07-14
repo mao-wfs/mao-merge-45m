6db4336080eb83da1896eed6b19b185c9587cee2
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2021-07-14 17:23:12 +0900

    #19 Replace test GBD data with that taken in Nov., 2020

--- tests/test_accelerometer.py
+++ tests/test_accelerometer.py
@@ -8,7 +8,7 @@ from nro45_merge.accelerometer import get_data, get_header
 
 
 # constants
-TEST_GBD = Path("data") / "2018-12-02_09_58_42.gbd"
+TEST_GBD = Path("data") / "20201123T004000.gbd"
 
 
 # test functions
@@ -16,7 +16,7 @@ def test_get_header():
     """Test whether the header of a GBD file is correctly parsed."""
     header = get_header(TEST_GBD)
 
-    assert header["Common"]["ID"] == "3502111C"  # first item
+    assert header["Common"]["ID"] == 44171012  # first item
     assert header["Waveform"]["XYLine"]["CH4"] == [21, 0, 21]  # last item
 
 
@@ -24,9 +24,9 @@ def test_get_data():
     """Test whether the data of a GBD file is correctly parsed."""
     data = get_data(TEST_GBD)
 
-    assert data.columns[0] == "CH1 (mV)"
+    assert data.columns[0] == "CH1 (V)"
     assert data.columns[-1] == "Status (None)"
-    assert data.index[0] == pd.Timestamp("2018-12-02 09:58:42.000000")
-    assert data.index[-1] == pd.Timestamp("2018-12-02 10:28:42.010000")
-    assert data["CH1 (mV)"][0] == -3.025
-    assert data["CH1 (mV)"][-1] == 30.725
+    assert data.index[0] == pd.Timestamp("2020-11-23 09:40:00.000000")
+    assert data.index[-1] == pd.Timestamp("2020-11-23 09:44:59.990000")
+    assert data["CH1 (V)"][0] == -0.350
+    assert data["CH1 (V)"][-1] == 1.300
