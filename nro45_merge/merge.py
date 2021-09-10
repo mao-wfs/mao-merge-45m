acd47edcc69ea82f241706c6c1eb6573be886288
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2021-09-10 17:30:21 +0900

    #27 Update merge function

--- nro45_merge/merge.py
+++ nro45_merge/merge.py
@@ -16,6 +16,7 @@ def merge(
     path_correlator_zarr: Path,
     path_accelerometer_zarr: Path,
     path_weather_zarr: Path,
+    path_antenna_zarr: Path,
     path_merge_zarr: Optional[Path] = None,
     interpolation: str = "linear",
     in_place: bool = False,
@@ -28,6 +29,7 @@ def merge(
         path_correlator_zarr: Path of the correlator Zarr file.
         path_accelerometer_zarr: Path of the accelerometer Zarr file.
         path_weather_zarr: Path of the weather Zarr file.
+        path_antenna_zarr: Path of the antenna Zarr file.
         path_merge_zarr: Path of the merge Zarr file (optional).
         interpolation: Method of interpolation of log data.
         in_place: When True, log data are added to the correlator Zarr file.
@@ -56,14 +58,17 @@ def merge(
     correlator = xr.open_zarr(path_correlator_zarr)
     accelerometer = xr.open_zarr(path_accelerometer_zarr)
     weather = xr.open_zarr(path_weather_zarr)
+    antenna = xr.open_zarr(path_antenna_zarr)
 
     # move data variables to coordinates
     accelerometer = xr.Dataset(coords=accelerometer.variables)
     weather = xr.Dataset(coords=weather.variables)
+    antenna = xr.Dataset(coords=antenna.variables)
 
     # interpolate log data to fit the time axis of the correlator
     accelerometer = accelerometer.interp_like(correlator, interpolation)
     weather = weather.interp_like(correlator, interpolation)
+    antenna = antenna.interp_like(correlator, interpolation)
 
     # append log data to the correlator or create the merge Zarr file
     if progress:
@@ -74,10 +79,12 @@ def merge(
     with bar:
         if in_place:
             accelerometer = accelerometer.merge(weather)
+            accelerometer = accelerometer.merge(antenna)
             accelerometer.to_zarr(path_correlator_zarr, mode="a")
         else:
             correlator = correlator.merge(accelerometer)
             correlator = correlator.merge(weather)
+            correlator = correlator.merge(antenna)
             correlator.to_zarr(path_merge_zarr)
 
     return path_merge_zarr
