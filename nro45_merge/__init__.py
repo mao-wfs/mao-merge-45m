e943f023672a80d400ed38fdf4c385bbfbc5faf8
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2021-09-16 15:15:35 +0900

    #27 Add import of submodules

--- nro45_merge/__init__.py
+++ nro45_merge/__init__.py
@@ -1,3 +1,11 @@
 # flake8: noqa
 __author__ = "Akio Taniguchi"
 __version__ = "0.1.0"
+
+
+# submodules
+from . import accelerometer
+from . import antenna
+from . import correlator
+from . import merge
+from . import weather
