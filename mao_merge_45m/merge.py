f053e9393f84137eaa1523c4cb2e5ea20d6e6488
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2022-09-26 02:45:38 +0000

    #59 Remove in-place option from merge function

--- mao_merge_45m/merge.py
+++ mao_merge_45m/merge.py
@@ -14,13 +14,12 @@ from dask.diagnostics import ProgressBar
 # main features
 def merge(
     path_correlator_zarr: Path,
+    path_merge_zarr: Optional[Path] = None,
     path_accelerometer_zarr: Optional[Path] = None,
     path_weather_zarr: Optional[Path] = None,
     path_antenna_zarr: Optional[Path] = None,
     path_sam45_zarr: Optional[Path] = None,
-    path_merge_zarr: Optional[Path] = None,
     interpolation: str = "linear",
-    in_place: bool = False,
     overwrite: bool = False,
     progress: bool = False,
 ) -> Path:
@@ -28,39 +27,32 @@ def merge(
 
     Args:
         path_correlator_zarr: Path of the correlator Zarr file.
+        path_merge_zarr: Path of the formatted Zarr file.
         path_accelerometer_zarr: Path of the accelerometer Zarr file.
         path_weather_zarr: Path of the weather Zarr file.
         path_antenna_zarr: Path of the antenna Zarr file.
         path_sam45_zarr: Path of the SAM45 Zarr file.
-        path_merge_zarr: Path of the merge Zarr file.
         interpolation: Method of interpolation of log data.
-        in_place: When True, log data are added to the correlator Zarr file.
         overwrite: Whether to overwrite the merge Zarr file if exists.
         progress: Whether to show a progress bar.
 
     Returns:
-        Path of the formatted Zarr file.
+        Path of the merge Zarr file.
 
     Raises:
-        FileExistsError: Raised if the formatted Zarr file exists
+        FileExistsError: Raised if the merge Zarr file exists
             and overwriting is not allowed (default).
 
     """
-    # check the existence of the merge Zarr file
-    if in_place:
-        path_merge_zarr = path_correlator_zarr
-
     if path_merge_zarr is None:
-        raise ValueError("Path of merge Zarr file is not specified.")
+        path_merge_zarr = path_correlator_zarr.with_suffix(".merged.zarr")
 
-    if not in_place and not overwrite and path_merge_zarr.exists():
+    if path_merge_zarr.exists() and not overwrite:
         raise FileExistsError(f"{path_merge_zarr} already exists.")
 
     # create (overwrite) the merge Zarr
     correlator = xr.open_zarr(path_correlator_zarr)
-
-    if not in_place:
-        correlator.to_zarr(path_merge_zarr, mode="w")
+    correlator.to_zarr(path_merge_zarr, mode="w")
 
     # append the other Zarrs to the merge Zarr
     for path in (
