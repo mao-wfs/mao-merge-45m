cf587bcb3075154f62381119105b75b1fd6fa445
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2022-09-26 10:43:41 +0000

    #59 Rename argument (path_merge_zarr → path_merged_zarr)

--- mao_merge_45m/merge.py
+++ mao_merge_45m/merge.py
@@ -14,7 +14,7 @@ from dask.diagnostics import ProgressBar
 # main features
 def merge(
     path_correlator_zarr: Path,
-    path_merge_zarr: Optional[Path] = None,
+    path_merged_zarr: Optional[Path] = None,
     path_accelerometer_zarr: Optional[Path] = None,
     path_weather_zarr: Optional[Path] = None,
     path_antenna_zarr: Optional[Path] = None,
@@ -27,34 +27,34 @@ def merge(
 
     Args:
         path_correlator_zarr: Path of the correlator Zarr file.
-        path_merge_zarr: Path of the formatted Zarr file.
+        path_merged_zarr: Path of the merged Zarr file.
         path_accelerometer_zarr: Path of the accelerometer Zarr file.
         path_weather_zarr: Path of the weather Zarr file.
         path_antenna_zarr: Path of the antenna Zarr file.
         path_sam45_zarr: Path of the SAM45 Zarr file.
         interpolation: Method of interpolation of log data.
-        overwrite: Whether to overwrite the merge Zarr file if exists.
+        overwrite: Whether to overwrite the merged Zarr file if exists.
         progress: Whether to show a progress bar.
 
     Returns:
-        Path of the merge Zarr file.
+        Path of the merged Zarr file.
 
     Raises:
-        FileExistsError: Raised if the merge Zarr file exists
+        FileExistsError: Raised if the merged Zarr file exists
             and overwriting is not allowed (default).
 
     """
-    if path_merge_zarr is None:
-        path_merge_zarr = path_correlator_zarr.with_suffix(".merged.zarr")
+    if path_merged_zarr is None:
+        path_merged_zarr = path_correlator_zarr.with_suffix(".merged.zarr")
 
-    if path_merge_zarr.exists() and not overwrite:
-        raise FileExistsError(f"{path_merge_zarr} already exists.")
+    if path_merged_zarr.exists() and not overwrite:
+        raise FileExistsError(f"{path_merged_zarr} already exists.")
 
-    # create (overwrite) the merge Zarr
+    # create (overwrite) the merged Zarr
     correlator = xr.open_zarr(path_correlator_zarr)
-    correlator.to_zarr(path_merge_zarr, mode="w")
+    correlator.to_zarr(path_merged_zarr, mode="w")
 
-    # append the other Zarrs to the merge Zarr
+    # append the other Zarrs to the merged Zarr
     for path in (
         path_accelerometer_zarr,
         path_weather_zarr,
@@ -69,8 +69,8 @@ def merge(
 
         if progress:
             with ProgressBar():
-                ds.to_zarr(path_merge_zarr, mode="a")
+                ds.to_zarr(path_merged_zarr, mode="a")
         else:
-            ds.to_zarr(path_merge_zarr, mode="a")
+            ds.to_zarr(path_merged_zarr, mode="a")
 
-    return path_merge_zarr
+    return path_merged_zarr
